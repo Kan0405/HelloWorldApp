1c7ff2e760d15244cca7d4b5660bd11df31b3757
Sergey <KononenkoSergey@mail.ru>
2020-08-26 17:57:49 +0300

    Initial Commit

---
+++ HelloWorldApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorldApp
+//
+//  Created by Сергей Кононенко on 26.08.2020.
+//  Copyright © 2020 Сергей Кононенко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
